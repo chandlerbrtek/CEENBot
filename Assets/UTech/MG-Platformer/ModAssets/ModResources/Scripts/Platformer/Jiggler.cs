a2cd301b90c2238d2f180fe72735f1025b05bc6d
chandler.brtek <chandler.brtek@proxibid.com>
2019-09-22 09:39:05 -0500

    added folders

--- Assets/UTech/MG-Platformer/ModAssets/ModResources/Scripts/Platformer/Jiggler.cs
+++ Assets/UTech/MG-Platformer/ModAssets/ModResources/Scripts/Platformer/Jiggler.cs
@@ -33,9 +33,9 @@ public class Jiggler : MonoBehaviour
     Vector3 baseScale;
 
     void Start(){
-        basePosition = transform.localPosition;
-        baseRotation = transform.localRotation;
-        baseScale = transform.localScale;
+        basePosition = this.transform.localPosition;
+        baseRotation = this.transform.localRotation;
+        baseScale = this.transform.localScale;
     }
 
     // Update is called once per frame
@@ -46,13 +46,8 @@ public class Jiggler : MonoBehaviour
         rotationTime += dt * rotationFrequency;
         scaleTime += dt * scaleFrequency;
 
-        if (jigPosition)
-        transform.localPosition = basePosition + positionJigAmount * Mathf.Sin(positionTime) * power;
-
-        if (jigRotation)
-        transform.localRotation = baseRotation * Quaternion.Euler(rotationJigAmount * Mathf.Sin(positionTime) * power);
-
-        if (jigScale)
-        transform.localScale = baseScale + scaleJigAmount * Mathf.Sin(scaleTime) * power;
+        this.transform.localPosition = basePosition + positionJigAmount * Mathf.Sin(positionTime) * power;
+        this.transform.localRotation = baseRotation * Quaternion.Euler(rotationJigAmount * Mathf.Sin(positionTime) * power);
+        this.transform.localScale = baseScale + scaleJigAmount * Mathf.Sin(scaleTime) * power;
     }
 }

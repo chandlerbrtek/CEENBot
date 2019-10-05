113738cc44550a514f76c4f16e7feab41d6ec8ca
NobleOrder <46690637+NobleOrder@users.noreply.github.com>
2019-10-05 15:11:27 -0500

    MenuScene Added

---
+++ src/Assets/Scripts/gameVolumeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameVolumeManager : MonoBehaviour
+{
+
+    public AudioSource branchSFX;
+    public AudioSource[] houseSFX;
+
+    void Start()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("DestructableTree"))
+        {
+            branchSFX.Play();
+        }
+
+        if (collision.gameObject.CompareTag("DestructableBuilding"))
+        {
+            int temp = Random.Range(0, 4);
+            houseSFX[temp].Play();
+        }
+    }
+}
---
+++ src/Assets/Scripts/volumeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeManager : MonoBehaviour {
+
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public AudioSource musicAudio;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        musicAudio.volume = musicSlider.value;
+        AudioListener.volume = masterSlider.value;
+	}
+}

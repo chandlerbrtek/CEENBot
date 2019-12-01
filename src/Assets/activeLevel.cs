af48a83b898eee684bb47cbf1105451a480858f3
NobleOrder <46690637+NobleOrder@users.noreply.github.com>
2019-12-01 14:44:24 -0600

    Joey
    level set/get

---
+++ src/Assets/activeLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activeLevel : MonoBehaviour
+{
+
+    static int currentLevel; 
+   
+    public void setLevel(int level)
+    {
+        currentLevel = level;
+    }
+
+    public int getLevel()
+    {
+        return currentLevel;
+    }
+}

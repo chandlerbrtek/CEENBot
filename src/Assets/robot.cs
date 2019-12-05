610eb69563124016db70a0cbb8bb205ad3da16c9
Sean <smcclenathan@gmail.com>
2019-12-04 21:01:46 -0600

    aspect ratio & smooth turning
    reduced field from 5x7 to 5x6 in order to have a better aspect ratio
    implemented smooth turning

--- src/Assets/robot.cs
+++ src/Assets/robot.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class robot : MonoBehaviour
 {
-    public Vector3 [] directions;
+    public Vector3[] directions;
     public int dir = 0;
     Vector3 rotation;
     public Vector3 position;
     public Vector3 start;
     // Start is called before the first frame update
+    bool rotating = true;
+    float startDeg;
+    float targetDeg;
+    float speed;
+    float turned;
+
     void Start()
     {
         directions = new Vector3[4];
-         directions[0] = new Vector3(1, 0, 0);
+        directions[0] = new Vector3(1, 0, 0);
         directions[1] = new Vector3(0, -1, 0);
         directions[2] = new Vector3(-1, 0, 0);
         directions[3] = new Vector3(0, 1, 0);
@@ -25,23 +31,43 @@ public class robot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (rotating)
+        {
+            rotation.z += speed;
+            this.transform.Rotate(new Vector3(0, 0, speed), Space.Self);
+            turned += speed;
+            if (Mathf.Abs(turned) == 90)
+            {
+                rotating = false;
+            }
+        }
     }
 
     public void right()
     {
+        /*
         rotation.z -= 90;
-        this.transform.Rotate(new Vector3(0,0,-90), Space.Self);
+        this.transform.Rotate(new Vector3(0,0,-90), Space.Self);*/
+        rotating = true;
+        startDeg = rotation.z;
+        targetDeg = startDeg - 90;
+        speed = -4.5f;
+        turned = 0;
         dir++;
         if (dir > 3)
             dir = 0;
-        
+
     }
 
     public void left()
     {
-        rotation.z += 90;
-        this.transform.Rotate(new Vector3(0,0,90), Space.Self);
+        /*rotation.z += 90;
+        this.transform.Rotate(new Vector3(0, 0, 90), Space.Self);*/
+        rotating = true;
+        startDeg = rotation.z;
+        targetDeg = startDeg + 90;
+        speed = 4.5f;
+        turned = 0;
         dir--;
         if (dir < 0)
             dir = 3;
@@ -59,11 +85,11 @@ public class robot : MonoBehaviour
         //this.transform.position = this.transform.position - directions[dir]*2;
         position -= directions[dir];
     }
-    
+
     public void setPos(Vector3 pos)
     {
         this.transform.position = pos;
-        
+
     }
 
     public void setStart(Vector3 pos)
@@ -115,6 +141,13 @@ public class robot : MonoBehaviour
         position = new Vector3(0, 0, 0);
         this.transform.position = start;
         while (dir != 0)
-            right();
+        {
+            rotation.z -= 90;
+            this.transform.Rotate(new Vector3(0, 0, -90), Space.Self);
+            dir++;
+            if (dir > 3)
+                dir = 0;
+
+        }
     }
 }
